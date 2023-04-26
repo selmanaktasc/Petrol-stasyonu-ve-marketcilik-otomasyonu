e5f269716ada23aa7f27d5a2e0d87a80a9a7552f
selmanaktasc <selmanakdasc65@gmail.com>
2023-04-26 23:27:51 +0300

    Depo Bilgileri kısmı güncellendi

--- Petrol İstasyonu ve marketcilik otomasyonu/Form1.Designer.cs
+++ Petrol İstasyonu ve marketcilik otomasyonu/Form1.Designer.cs
@@ -35,10 +35,12 @@
             this.progressBar3 = new System.Windows.Forms.ProgressBar();
             this.progressBar2 = new System.Windows.Forms.ProgressBar();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label6 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
@@ -56,7 +58,7 @@
             this.tabControl1.Location = new System.Drawing.Point(0, -1);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(764, 293);
+            this.tabControl1.Size = new System.Drawing.Size(785, 353);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
@@ -66,10 +68,12 @@
             this.tabPage1.Controls.Add(this.progressBar3);
             this.tabPage1.Controls.Add(this.progressBar2);
             this.tabPage1.Controls.Add(this.progressBar1);
+            this.tabPage1.Controls.Add(this.label6);
             this.tabPage1.Controls.Add(this.label4);
             this.tabPage1.Controls.Add(this.label8);
+            this.tabPage1.Controls.Add(this.label9);
             this.tabPage1.Controls.Add(this.label7);
-            this.tabPage1.Controls.Add(this.textBox4);
+            this.tabPage1.Controls.Add(this.textBox3);
             this.tabPage1.Controls.Add(this.textBox2);
             this.tabPage1.Controls.Add(this.textBox1);
             this.tabPage1.Controls.Add(this.label3);
@@ -78,7 +82,7 @@
             this.tabPage1.Location = new System.Drawing.Point(4, 25);
             this.tabPage1.Name = "tabPage1";
             this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(756, 264);
+            this.tabPage1.Size = new System.Drawing.Size(777, 324);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "DEPO BİLGİLERİ";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -86,41 +90,50 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(77, 20);
+            this.label5.Location = new System.Drawing.Point(91, 16);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(165, 16);
+            this.label5.Size = new System.Drawing.Size(183, 16);
             this.label5.TabIndex = 8;
-            this.label5.Text = "depodaki akaryakıt miktarı";
+            this.label5.Text = "mevcut akaryakıt miktarı(Litre)";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(602, 204);
+            this.button1.Location = new System.Drawing.Point(630, 272);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(141, 46);
             this.button1.TabIndex = 7;
             this.button1.Text = "depoyu güncelle";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // progressBar3
             // 
-            this.progressBar3.Location = new System.Drawing.Point(418, 152);
+            this.progressBar3.Location = new System.Drawing.Point(446, 152);
             this.progressBar3.Name = "progressBar3";
             this.progressBar3.Size = new System.Drawing.Size(325, 23);
             this.progressBar3.TabIndex = 6;
             // 
             // progressBar2
             // 
-            this.progressBar2.Location = new System.Drawing.Point(418, 100);
+            this.progressBar2.Location = new System.Drawing.Point(446, 104);
             this.progressBar2.Name = "progressBar2";
             this.progressBar2.Size = new System.Drawing.Size(325, 23);
             this.progressBar2.TabIndex = 6;
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(418, 56);
+            this.progressBar1.Location = new System.Drawing.Point(446, 56);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(325, 23);
             this.progressBar1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(20, 194);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 16);
+            this.label6.TabIndex = 5;
             // 
             // label4
             // 
@@ -140,6 +153,15 @@
             this.label8.TabIndex = 5;
             this.label8.Text = "DİZEL";
             // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(318, 16);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(89, 16);
+            this.label9.TabIndex = 5;
+            this.label9.Text = "Eklenen(Litre)";
+            // 
             // label7
             // 
             this.label7.AutoSize = true;
@@ -149,23 +171,23 @@
             this.label7.TabIndex = 5;
             this.label7.Text = "BENZİN";
             // 
-            // textBox4
+            // textBox3
             // 
-            this.textBox4.Location = new System.Drawing.Point(272, 156);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(100, 22);
-            this.textBox4.TabIndex = 4;
+            this.textBox3.Location = new System.Drawing.Point(307, 153);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 4;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(272, 107);
+            this.textBox2.Location = new System.Drawing.Point(307, 105);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(100, 22);
             this.textBox2.TabIndex = 4;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(272, 57);
+            this.textBox1.Location = new System.Drawing.Point(307, 56);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 3;
@@ -173,7 +195,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(136, 156);
+            this.label3.Location = new System.Drawing.Point(158, 158);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(44, 16);
             this.label3.TabIndex = 2;
@@ -182,7 +204,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(136, 107);
+            this.label2.Location = new System.Drawing.Point(158, 107);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 1;
@@ -191,7 +213,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(136, 60);
+            this.label1.Location = new System.Drawing.Point(158, 60);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 16);
             this.label1.TabIndex = 0;
@@ -202,7 +224,7 @@
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(756, 264);
+            this.tabPage2.Size = new System.Drawing.Size(777, 324);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "FİYAT BİLGİLERİ";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -211,10 +233,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(758, 286);
+            this.ClientSize = new System.Drawing.Size(790, 354);
             this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
-            this.Text = "Depo Bİlgileri";
+            this.Text = "Petrol İstasyonu ve Marketcilik Otomasyonu";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
@@ -239,9 +261,11 @@
         private System.Windows.Forms.ProgressBar progressBar2;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label9;
     }
 }
 
